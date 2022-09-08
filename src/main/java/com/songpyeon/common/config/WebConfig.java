cdd4293c10c8690ae90e8fe3d02373d6824c953b
park0503 <hooo0503@gmail.com>
2022-09-09 01:13:14 +0900

    chore: cors 설정

---
+++ src/main/java/com/songpyeon/common/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.songpyeon.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/v1/**")
+        .allowedOrigins("*")
+        .allowedMethods(
+            HttpMethod.GET.name(),
+            HttpMethod.POST.name()
+        )
+        .allowCredentials(true);
+  }
+}
