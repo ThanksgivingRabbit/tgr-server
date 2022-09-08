d5bff22b95b1aca12125aa9571c28ca6eb7bd159
park0503 <hooo0503@gmail.com>
2022-09-09 01:04:51 +0900

    init: server 초기화

---
+++ src/main/java/com/songpyeon/SongpyeonApplication.java
@@ -0,0 +1,13 @@
+package com.songpyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongpyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongpyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/songpyeon/SongpyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songpyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongpyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
