eb2f4ded3812aad8c2b206a5b426dff74e50c01e
park0503 <hooo0503@gmail.com>
2022-09-11 16:23:53 +0900

    fix: cors 수정

--- src/main/java/com/tgr/common/config/WebConfig.java
+++ src/main/java/com/tgr/common/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/api/v1/**")
-        .allowedOrigins("/**")
+        .allowedOrigins("*")
         .allowedMethods(
             HttpMethod.GET.name(),
             HttpMethod.POST.name()
